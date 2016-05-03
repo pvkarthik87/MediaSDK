8f13161e6fdda99800622259fb21dadafb8f9772
Venkata Karthik Paruchuri <pvkarthik87@gmail.com>
2016-05-03 08:35:01 +0200

    Changed DASH source to smurfed in demo app, increased minor version to 9 in mediaservice.

--- app/src/main/java/com/kar/mediasdk/Constants.java
+++ app/src/main/java/com/kar/mediasdk/Constants.java
@@ -5,10 +5,12 @@ package com.kar.mediasdk;
  */
 public interface Constants {
 
-    String DASH_SAMPLE_URI = "http://www.youtube.com/api/manifest/dash/id/3aa39fa2cc27967f/source/youtube?"
+    /*String DASH_SAMPLE_URI = "http://www.youtube.com/api/manifest/dash/id/3aa39fa2cc27967f/source/youtube?"
             + "as=fmp4_audio_clear,fmp4_sd_hd_clear&sparams=ip,ipbits,expire,source,id,as&ip=0.0.0.0&"
             + "ipbits=0&expire=19000000000&signature=A2716F75795F5D2AF0E88962FFCD10DB79384F29."
-            + "84308FF04844498CE6FBCE4731507882B8307798&key=ik0";
+            + "84308FF04844498CE6FBCE4731507882B8307798&key=ik0";*/
+
+    String DASH_SAMPLE_URI = "http://demo.unified-streaming.com/video/smurfs/smurfs.ism/smurfs.mpd";
 
     String DASH_SAMPLE_CONTENT_ID = "Google Play (MP4,H264)";
 
